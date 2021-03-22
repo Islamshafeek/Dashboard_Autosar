eeb5105a8ae5c2fec647d45268a7286e87e4a9b1
rdwaz7 <rdwaz7@gmail.com>
2021-03-22 10:58:26 +0200

    Committing Lib Folder

---
+++ Timer_Driver_Module/Lib/Status.h
@@ -0,0 +1,20 @@
+/*
+ * status.h
+ *
+ *  Created on: Feb 3, 2021
+ *      Author: Rdwa Salah
+ */
+
+#ifndef STATUS_H_
+#define STATUS_H_
+
+typedef enum
+{
+	RT_SUCCESS,
+	RT_ERROR,
+	RT_PARAM
+}status_RT;
+
+
+
+#endif /* STATUS_H_ */
